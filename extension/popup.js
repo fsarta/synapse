d0c7dd66993007ad57b09d9c5e80085e35da0a71
fsarta <francesco.sarta@gmail.com>
2026-01-14 16:38:40 +0100

    modified popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const button = document.getElementById('open-dashboard');
   if (button) {
     button.addEventListener('click', function() {
-      window.open('https://synapse-dashboard.vercel.app', '_blank');
+      window.open('https://synapse-dashboard-r6xoeckq4-frasars-projects.vercel.app/', '_blank');
     });
   }
 });
